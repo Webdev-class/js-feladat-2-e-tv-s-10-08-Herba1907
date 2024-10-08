80530477f8c0eef6fb070c0db99b3183558cbe47
Herba1907 <hubaherberth@gmail.com>
2024-10-08 13:14:33 +0200

    helo

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,12 @@
-
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const locationss = require('../locations.json')
+const APIAdress = 'https://archive-api.open-meteo.com/v1/archive';
 async function f1 (lat, long, timeStr) {
-    //TODO
+    let sum=0;
+    let datakaka;
+    await fetch(APIAdress+`?latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`)
+    .then(data=>data.json()).then(data => datakaka=data).then(data=> console.log(data));
+    datakaka.hourly.temperature_2m.forEach(element => {
+        sum+=element;});
+     return Math.floor(sum/24);
 }
-
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -1,6 +1,9 @@
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const APIAdress = 'https://archive-api.open-meteo.com/v1/archive';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    //TODO
+    let datakaka;
+    await fetch(APIAdress+`?latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`)
+    .then(data=>data.json()).then(data => datakaka=data).then(data=> console.log(data));
+     return datakaka.hourly.temperature_2m.at(hour)
 }
 module.exports = f2;
